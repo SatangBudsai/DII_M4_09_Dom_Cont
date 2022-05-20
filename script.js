03e31e5aad984ea98c19fe4eaaa75012faed25e1
Satang Budsai <Satang_budsai@cmu.ac.th>
2022-05-20 11:57:05 +0700

    addStudent in array

--- script.js
+++ script.js
@@ -2,16 +2,35 @@ var student = {};
 student.name = 'คุณลุง';
 student.username = 'a@b.com';
 student.gender = 'ชาย';
-console.log(student.name)
-
+// console.log(student.name)
 // document.getElementById('output').innerText = student;
 
+var secondStudent = {};
+secondStudent.name = 'คุณนาย';
+secondStudent.username = 'c@f.com';
+secondStudent.gender = 'หญิง';
+
+var students = [
+    student,secondStudent,{
+        name :'สมรักษ์',
+        username : 'm@n.com',
+        gender : 'ชาย',
+    }
+]
 
 function addStudent(student){
     const output = document.getElementById('output');
     addStudentData(output,'ชื่อ',student.name);
     addStudentData(output,'รหัส',student.username);
     addStudentData(output,'เพศ',student.gender);
+
+    addStudentData(output,'ชื่อ',secondStudent.name);
+    addStudentData(output,'รหัส',secondStudent.username);
+    addStudentData(output,'เพศ',secondStudent.gender);
+    
+    addStudentData(output,'ชื่อ',students[2].name);
+    addStudentData(output,'รหัส',students[2].username);
+    addStudentData(output,'เพศ',students[2].gender);
 }
 function addStudentData(type,key,data){
     let row = document.createElement('div');
